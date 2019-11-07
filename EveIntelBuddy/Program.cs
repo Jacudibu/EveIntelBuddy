8769daab4ddf72470a095e6f7bf2a6a90bd958c8
Jacudibu <jacudibu@gmail.com>
2019-11-07 12:27:27 +0100

    Using PC Speaker beep sound when something is detected

--- EveIntelBuddy/Program.cs
+++ EveIntelBuddy/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 
@@ -7,7 +8,9 @@ namespace EveIntelBuddy
     class Program
     {
         private const string FilePath = "/ssdapps/eve-online/drive_c/users/jacudibu/My Documents/EVE/logs/Chatlogs/";
-        private const string ChannelName = "Test123Yay";
+        private const string ChannelName = "Bean-Intel";
+        private static List<string> _watchedSystems = new List<string>() {"6V-D0E", "LS3-HP", "QX-4HO", "BVRQ-O"};
+        private const int BeepDurationInSeconds = 1;
         
         private static FileStream? _intelFileStream;
         private static StreamReader? _intelStreamReader;
@@ -53,7 +56,6 @@ namespace EveIntelBuddy
         
         static void OnChanged(object source, FileSystemEventArgs e)
         {
-            Console.Out.WriteLine("OnChanged triggered " + e.Name);
             if (_intelFileStream == null)
             {
                 _intelFileStream = new FileStream(e.FullPath, FileMode.Open, FileAccess.Read, FileShare.Read);
@@ -63,11 +65,35 @@ namespace EveIntelBuddy
             {
                 _intelStreamReader = new StreamReader(_intelFileStream);
             }
+
+            if (e.FullPath != _intelFileStream.Name)
+            {
+                return;
+            }
+            
+            Console.Out.WriteLine("OnChanged triggered " + e.Name);
             
             while (!_intelStreamReader.EndOfStream)
             {
-                var data = _intelStreamReader.ReadLine();
-                Console.Out.WriteLine(data);
+                var line = _intelStreamReader.ReadLine();
+                ProcessLine(line);
+            }
+        }
+
+        static void ProcessLine(string line)
+        {
+            Console.Out.WriteLine(line);
+            foreach (var systemName in _watchedSystems)
+            {
+                if (line.ToLower().Contains(systemName.ToLower()))
+                {
+                    Console.Out.WriteLine(line);
+                    for (int i = 0; i < BeepDurationInSeconds * 5; i++)
+                    {
+                        Console.Beep();
+                        Thread.Sleep(200);                        
+                    }
+                }
             }
         }
     }
